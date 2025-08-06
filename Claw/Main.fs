bc099ef6c6cc70b9f103d292c32acd415b865a24
Michael Shaulskiy <mshaulskiy@outlook.com>
2025-08-06 18:10:31 +0200

    Removed leftover myriad config

--- Claw/Main.fs
+++ Claw/Main.fs
@@ -6,6 +6,9 @@ open Claw.Core.Prelude
 
 [<EntryPoint>]
 let main argv = 
+    printfn "Demonstrating 'where' binding in F#..."
+    
+    // Original REPL functionality
+    printfn "\nStarting REPL (type EXIT to quit)..."
     repl (fun () -> System.Console.ReadLine()) |> ignore
-    stripWhiteSpace $ unpackMaybeParseResult (executeTokenParser "#define MYVALUE a  ") |> ignore
     0
