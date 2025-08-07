4e8e20b26a0b19ab963a2f22fa19e4707d99c0ff
Michael Shaulskiy <mshaulskiy@outlook.com>
2025-08-07 15:28:46 +0200

    Enabled NativeAOT and more work on parser

--- Claw.Core/Parser.fs
+++ Claw.Core/Parser.fs
@@ -12,4 +12,13 @@ type ParseTree<'T> =
     | PNode of 'T * List<ParseTree<'T>>
     | PLeaf of 'T
 
-let pdeclparse: ParseTree<Token> = 
+type PExpression = 
+    | DeclareStmt
+
+let pdirectivedefineparse (input: Token list): ParseTree<PExpression> =
+    let pdefineboolimpl = fun tokens -> match tokens with
+                                            | Directive Define Identifier ident :: [] ->   
+    match input with
+    | Directive Define :: xs -> PLeaf (DeclareStmt)
+
+// let pdeclparse: ParseTree<Token> = 
